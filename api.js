5d6d82b35750b30bb8041e9c289f1f31b3c4fcfa
Chioma Okeke <omaokeke28@gmail.com>
2021-01-06 23:45:29 +0100

    more updates

--- api.js
+++ api.js
@@ -11,16 +11,37 @@ var notification =  require('./controllers/notificationController');
 var payment = require('./controllers/paymentController');
 var advert = require('./controllers/advertController');
 // var upload = require('./controllers/uploadDocumentController');
+const PORT = process.env.PORT || 8600
 
 var corsOption = {
-    origin : ["https://naijayellowcatalogue.netlify.app", "http://localhost","http://localhost:8600"],
+    origin : ["https://naijayellowcatalogue.netlify.app", "https://warm-journey-85178.herokuapp.com",
+    "http://localhost:8600"],
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     optionSuccessStatus: 200
 }
 app = express();
-app.use(bodyParser(),cors(corsOption));
+app.use(bodyParser.json(),cors(corsOption));
 
+app.get('/', (req, res) => {
+    res.send("Welcome to yellow page")
+})
 
+app.use((req, res, next) => {
+    const error = new Error('Could not find this route');
+    throw error;
+})
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occurred!'});
+})
+
+app.get('/', (req, res) => {
+    res.send("Welcome to yellow page")
+})
 /*************************************** Instantiate Controllers **************************************/
 users(app);
 roles(app);
@@ -36,5 +57,5 @@ advert(app);
 
 /**************************************** Assign port *************************************************/
 
-app.listen(8600);
-console.log('Listening at 8600');
+app.listen(PORT);
+console.log(`Listening at ${PORT} `);
